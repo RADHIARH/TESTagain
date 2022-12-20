56dda5ad02961b8754dace0aa3c9240b80ada117
RADHIARH <radhia.rahmani@sesame.com.tn>
2022-12-20 13:17:04 +0100

    send

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,7 @@ let userList = [];
 // List of users
 exports.userList = function (request, response) {
   connection.query("Select * from employees", function (error, resultSQL) {
-    if (!error) response.send(resultSQL);
+    if (!error) response.json(resultSQL);
     else console.log(error);
   });
 };
