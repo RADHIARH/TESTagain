67bc37644c06a2931af2675428a8a134207054fc
RADHIARH <radhia.rahmani@sesame.com.tn>
2022-12-20 13:38:42 +0100

    send

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,6 @@
 let User = require("../models/userModel");
 let connection = require("../db");
-var mysql = require("mysql2");
-//Database connection
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "reactjs!nodejs$ingenieurinformatique",
-  database: "node_mysql_crud_db",
-});
+
 // List of users
 exports.userList = function (request, response) {
   connection.query("Select * from employees", function (error, resultSQL) {
